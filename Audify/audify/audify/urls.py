5b016a7038893c9458b8cde19917270ae07e6b2e
Himanshi <himanshipahuja14@gmail.com>
2021-12-19 15:27:12 +0530

    avatar and bio added

--- Audify/audify/audify/urls.py
+++ Audify/audify/audify/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django import urls
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path,include
@@ -32,3 +33,6 @@ urlpatterns = [
     path('register/',user_views.register, name='register' ),
     path('profile/',user_views.profile, name='profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
